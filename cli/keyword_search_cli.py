5114ec3eea9ea361e5a32594fdceda33da32018d
Adam James <adam.james870@yahoo.co.uk>
2025-12-03 04:15:04 +0000

    Adds basic keyword search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,10 @@
 ﻿#!/usr/bin/env python3
 
 import argparse
+import json
+from movies_model import MoviesData
+
+MOVIES_FILEPATH = "./data/movies.json"
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Keyword Search CLI')
@@ -13,10 +17,31 @@ def main() -> None:
 
     match args.command:
         case 'search':
-            print(f"Searching for: {args.query}")
+            found_movies = keyword_search(args.query, 5)
+            i = 1
+            for movie in found_movies.movies:
+                print(f'{i}. {movie.title}')
+                i += 1
             pass
         case _:
             parser.print_help()
+
+def keyword_search(keyword: str, max_results: int) -> MoviesData:
+
+    with open(MOVIES_FILEPATH, 'r') as file:
+        data = json.load(file)
+
+    movies_data = MoviesData.from_dict(data)
+    found_movies = MoviesData()
+
+    for this_movie in movies_data.movies:
+        if keyword in this_movie.title:
+            found_movies.movies.append(this_movie)
+        if len(found_movies.movies) >= max_results:
+            break
+
+    found_movies.movies = sorted(found_movies.movies, key=lambda movie: movie.id, reverse=True)
+    return found_movies
 
 if __name__ == '__main__':
     main()
---
+++ cli/movies_model.py
@@ -0,0 +1,34 @@
+﻿from typing import List
+from typing import Any
+from dataclasses import dataclass
+import json
+@dataclass
+class Movie:
+    id: int
+    title: str
+    description: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Movie':
+        _id = int(obj.get("id"))
+        _title = str(obj.get("title"))
+        _description = str(obj.get("description"))
+        return Movie(_id, _title, _description)
+
+@dataclass
+class MoviesData:
+    movies: List[Movie]
+
+    def __init__(self, movies=None):
+        if movies is None:
+            movies = []
+        self.movies = movies
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Root':
+        _movies = [Movie.from_dict(y) for y in obj.get("movies")]
+        return MoviesData(_movies)
+
+# Example Usage
+# jsonstring = json.loads(myjsonstring)
+# root = Root.from_dict(jsonstring)
